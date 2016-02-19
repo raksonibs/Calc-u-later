3f8236763caa9a1347b37961d6b3d2fd8584feb6
asmaaah <asma.ahassan@yahoo.com>
2016-02-19 14:41:07 -0500

    this is for model test

--- src/CalcModelTest.java
+++ src/CalcModelTest.java
@@ -1,4 +1,62 @@
+import static org.junit.Assert.*;
+
+import java.math.BigInteger;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class CalcModelTest {
 
+
+	private CalcModel c;
+	private BigInteger b;
+	
+	@Before
+	public void setUp() throws Exception {
+		c = new CalcModel();
+	}
+
+	@Test
+	public void testClear() {
+		c.clear();
+		assertEquals(b.ZERO,c.getCalcValue());
+	}
+
+	@Test
+	public void testSum() {
+		
+		c.sum(b.TEN);
+		assertEquals(b.TEN, c.getCalcValue());
+		
+	}
+	
+	@Test
+	public void testSubtract() {
+		
+		c.subtract(new BigInteger("2"));
+		assertEquals(new BigInteger("-2"), c.getCalcValue());
+		
+	}
+	
+	@Test
+	public void testMultiply() {
+		
+		c.multiply(new BigInteger("2"));
+		assertEquals(b.ZERO, c.getCalcValue());
+		
+	}
+	
+	@Test
+	public void testDivide() {
+		
+		c.divide(new BigInteger("2"));
+		assertEquals(b.ZERO, c.getCalcValue());
+		
+	}
+	
+
 }
+
+
+
+
