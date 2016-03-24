166163156b53e0aeaedaa0a90cf9f0b79816ccbb
Oskar Niburski <oskarniburski@gmail.com>
2016-03-23 23:28:47 -0400

    eventual tab portion

--- src/CalcView.java
+++ src/CalcView.java
@@ -14,11 +14,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.awt.GridBagLayout;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.ComponentOrientation;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.util.Stack;
+import javax.swing.JTabbedPane;
+import javax.swing.ImageIcon;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -44,11 +48,56 @@ public class CalcView extends JFrame
 		
 		addComponentsToPane(this, theController);
 		
+//		JTabbedPane tabbedPane = new JTabbedPane();
+//		ImageIcon icon = createImageIcon("images/calculator-icon.png");
+//
+//		JComponent panel1 = makeTextPanel("Panel #1");
+//		tabbedPane.addTab("Tab 1", icon, panel1,
+//		                  "Does nothing");
+//		tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+//
+//		JComponent panel2 = makeTextPanel("Panel #2");
+//		tabbedPane.addTab("Tab 2", icon, panel2,
+//		                  "Does twice as much nothing");
+//		tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+//
+//		JComponent panel3 = makeTextPanel("Panel #3");
+//		tabbedPane.addTab("Tab 3", icon, panel3,
+//		                  "Still does nothing");
+//		tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
+//
+//		JComponent panel4 = makeTextPanel(
+//		        "Panel #4 (has a preferred size of 410 x 50).");
+//		panel4.setPreferredSize(new Dimension(410, 50));
+//		tabbedPane.addTab("Tab 4", icon, panel4,
+//		                      "Does nothing at all");
+//		tabbedPane.setMnemonicAt(3, KeyEvent.VK_4);
+		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.pack();
 		this.setVisible(true);
 
 	}
+	
+	protected JComponent makeTextPanel(String text) {
+        JPanel panel = new JPanel(false);
+        JLabel filler = new JLabel(text);
+        filler.setHorizontalAlignment(JLabel.CENTER);
+        panel.setLayout(new GridBagLayout());
+        panel.add(filler);
+        return panel;
+    }
+     
+    /** Returns an ImageIcon, or null if the path was invalid. */
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = CalcView.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
 
 	@SuppressWarnings("serial")
 	public static void addComponentsToPane(Container pane, final CalcController theController) {
