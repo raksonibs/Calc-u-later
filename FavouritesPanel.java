f1d0feed083fb1c01d8849eb86d8ffe72dbb102b
Anji Tong <ollejna@gmail.com>
2016-03-25 19:37:31 -0400

    Added files via upload

---
+++ FavouritesPanel.java
@@ -0,0 +1,45 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class FavouritesPanel extends JPanel implements ActionListener
+ {
+   private int width, height;
+   JPanel graph = new JPanel();
+   Color color;
+   JTextField Function;
+   Graph temp;
+   public FavouritesPanel(int width1, int height1)
+    {
+       this.width = width1;
+       this.height = height1;
+       Function = new JTextField();
+       JButton startR = new JButton("Graph"); startR.addActionListener(this);
+       startR.setSize(new Dimension(10,10));
+       JPanel Buttons = new JPanel(new GridLayout(1,2));      
+       Buttons.setPreferredSize(new Dimension(500,20));
+       Buttons.add(Function);
+       Buttons.add(startR);
+       temp = new Graph();
+       graph.add(temp);
+
+
+      setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+   
+       add(graph);
+       add(Buttons);
+       //set preferred size of this panel
+       setPreferredSize(new Dimension(width,height));
+    }
+
+
+      public void actionPerformed(ActionEvent event)
+        {
+           String input = Function.getText().trim();
+           temp.graphs(-10,-10,10,10,color.blue,input);
+
+        } //end of actionPerformed method
+  
+} //end of ControlPanel class
+
