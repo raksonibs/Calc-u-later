35a803471d5da241f14e605e1e840059d2172cfc
Oskar Niburski <oskarniburski@gmail.com>
2016-03-25 22:30:11 -0400

    Example event listener

--- src/mainClass.java
+++ src/mainClass.java
@@ -1,30 +1,38 @@
-
-//Assignment #: 10
-
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 import java.awt.*;
 
 public class mainClass extends JApplet
 {
-	private JTabbedPane MAIN = new JTabbedPane();
-	public static int WIDTH = 800;
-	public static int HEIGHT = 400;
-	CalcModel model;
-	CalcController control = new CalcController();
+private JTabbedPane MAIN = new JTabbedPane();
+ public static int WIDTH = 800;
+ public static int HEIGHT = 400;
+ CalcModel model;
+ CalcController control = new CalcController();
 
-	public void init()
-	{
-		System.out.println("mainClass");
+public void init()
+{
+	System.out.println("mainClass");
 
-		GraphPanel panel = new GraphPanel(WIDTH, HEIGHT);
-		FavouritesPanel panel2 = new FavouritesPanel();
-		CalcView panel3 = new CalcView(control);
+    GraphPanel panel = new GraphPanel(WIDTH,HEIGHT);
+    FavouritesPanel panel2 = new FavouritesPanel();
+    CalcView panel3 = new CalcView(control);
+    
+    
+    
+    MAIN.addTab("Calculator", panel3);
+    MAIN.addTab("Graph",panel);
+    MAIN.addTab("Favourites", panel2);
+    
+    MAIN.addChangeListener(new ChangeListener() {
+        public void stateChanged(ChangeEvent e) {
+            System.out.println("Tab: " + MAIN.getSelectedIndex());
+        }
+    });
 
-		MAIN.addTab("Calculator", panel3);
-		MAIN.addTab("Graph", panel);
-		MAIN.addTab("Favourites", panel2);
-
-		getContentPane().add(MAIN);
-		setSize(WIDTH, HEIGHT);
-	}
+    getContentPane().add(MAIN);
+    setSize(WIDTH,HEIGHT);
+}
 }
