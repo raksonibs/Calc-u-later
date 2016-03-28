759c2fc6b70120caa19a65482833c49ffddbf9a5
zarifi <zarifi@my.yorku.ca>
2016-03-28 12:10:24 -0400

    least amount of brackets

--- src/CalcModel.java
+++ src/CalcModel.java
@@ -5,11 +5,14 @@ import java.math.BigInteger;
 import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 import java.util.Vector;
 
 import javax.swing.*;
+
+//import CalcModel.Parser.Result;
 
 import java.awt.*;
 import java.awt.event.*;
@@ -588,7 +591,26 @@ public class CalcModel
 		{
 		expression = expression.substring(1, expression.length());
 		}
-		return expression;
+		
+		if(expression.contains(",")){
+			System.out.println("this is expression"+expression);
+			List<String> exps = Arrays.asList(expression.split(","));
+			String result = "";
+			for (int i = 0; i < exps.size() ; i++){
+				System.out.println("array"+exps.get(i));
+				result += new Parser(exps.get(i)).parse();
+				result += ",";
+			}
+			return result;
+		}
+		else
+		{
+		Parser exp = new Parser(expression);
+		System.out.println("this is expression"+ expression);
+		return exp.parse();
+
+		}
+		//return expression;
 	}
 	
 	public String getLastExpression() {
@@ -830,7 +852,135 @@ public class CalcModel
 	
 	
 	
-	
+	public class Parser {
+	    private final static char EOF = ';';
+	    private String input;
+	    private int currPos;
+
+	    public Parser(String input) {
+	        this.input = input + EOF; // mark the end
+	        this.currPos = -1;
+	    }
+
+	    public String parse() throws IllegalArgumentException {
+	        nextToken();
+	        Result result = expression();
+	        if(currToken() != EOF) {
+	            throw new IllegalArgumentException("Found unexpected character '" + currToken() + "' at position " + currPos);
+	        }
+	        return result.getText();
+	    }
+
+	    // "expression()" handles "term" or "term + term" or "term - term"
+	    private Result expression() throws IllegalArgumentException {
+	        Result leftArg = term();
+
+	        char operator = currToken();
+	        if (operator != '+' && operator != '-') {
+	            return leftArg; // EXIT
+	        }
+	        nextToken();
+
+	        Result rightArg = term();
+
+	        if(operator == '-' && (rightArg.getOp() == '-' || rightArg.getOp() == '+')) {
+	            rightArg = encloseInParentheses(rightArg);
+	        }
+
+	        return new Result(leftArg.getText() + "" + operator + "" + rightArg.getText(), operator);
+	    }
+
+	    // "term()" handles "factor" or "factor * factor" or "factor / factor"
+	    private Result term() throws IllegalArgumentException {
+	        Result leftArg = factor();
+
+	        char operator = currToken();
+	        if (operator != '*' && operator != '/') {
+	            return leftArg; // EXIT
+	        }
+	        nextToken();
+
+	        Result rightArg = factor();
+
+	        if(leftArg.getOp() == '+' || leftArg.getOp() == '-') {
+	            leftArg = encloseInParentheses(leftArg);
+	        }
+	        if(rightArg.getOp() == '+' || rightArg.getOp() == '-' || (operator == '/' && (rightArg.getOp() == '/' || rightArg.getOp() == '*'))) {
+	            rightArg = encloseInParentheses(rightArg);
+	        }
+
+	        return new Result(leftArg.getText() + "" + operator + "" + rightArg.getText(), operator);
+	    }
+
+	    // "factor()" handles a "paren" or a "variable"
+	    private Result factor() throws IllegalArgumentException {
+	        Result result;
+	        if(currToken() == '(') {
+	            result = paren();
+	        } else if(Character.isDigit(currToken()) || currToken() == '?') {
+	            result = variable();
+	        } else {
+	            throw new IllegalArgumentException("Expected variable or '(', found '" + currToken() + "' at position " + currPos);
+	        }
+	        return result;
+	    }
+
+	    // "paren()" handles an "expression" enclosed in parentheses
+	    // Called with currToken an opening parenthesis
+	    private Result paren() throws IllegalArgumentException {
+	        nextToken();
+	        Result result = expression();
+	        if(currToken() != ')') {
+	            throw new IllegalArgumentException("Expected ')', found '" + currToken() + "' at position " + currPos);
+	        }
+	        nextToken();
+	        return result;
+	    }
+
+	    // "variable()" handles a variable
+	    // Called with currToken a variable
+	    private Result variable() throws IllegalArgumentException {
+	        Result result = new Result(Character.toString(currToken()), ' ');
+	        nextToken();
+	        return result;
+	    }
+
+	    private char currToken() {
+	        return input.charAt(currPos);
+	    }
+
+	    private void nextToken() {
+	        if(currPos >= input.length() - 1) {
+	            throw new IllegalArgumentException("Unexpected end of input");
+	        }
+	        do {
+	            ++currPos;
+	        }
+	        while(currToken() != EOF && currToken() == ' ');
+	    }
+
+	    private Result encloseInParentheses(Result result) {
+	        return new Result("(" + result.getText() + ")", result.getOp());
+	    }
+
+	    private class Result {
+	        private final String text;
+	        private final char op;
+
+	        private Result(String text, char op) {
+	            this.text = text;
+	            this.op = op;
+	        }
+
+	        public String getText() {
+	            return text;
+	        }
+
+	        public char getOp() {
+	            return op;
+	        }
+	    }
+	}
 	
 	
 	
