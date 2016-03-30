dc37574fc730cdc7511d2da90886533b9359af0c
Oskar Niburski <oskarniburski@gmail.com>
2016-03-29 23:32:51 -0400

    Almost done

--- app/js/main.js
+++ app/js/main.js
@@ -36,11 +36,22 @@ $('.parent-nav li').hover(function() {
 $('.smaller-nav-options').fadeOut();
 
 
-$.each([['#start', '#intro'], ["#intro", "#demo"], ["#demo", '#design'], ['#design', '#testing'], ['#testing', '#web'], ['#web', '#asmah'], ['#asmah', '#mohammad'], ["#mohammad", '#anji'], ['#anji', '#brad'], ['#brad', '#oskar'], ['#oskar', '#goals']], function(index, val) {
+$.each([['#start', '#intro'], ["#intro", "#idea"], ["#idea", "#demo"], ["#demo", '#design'], ['#design', '#testing'], ['#testing', '#web'], ['#web', '#asmah'], ['#asmah', '#mohammad'], ["#mohammad", '#anji'], ['#anji', '#brad'], ['#brad', '#oskar'], ['#oskar', '#goals']], function(index, val) {
+  
   $(val[0]).click(function() {
    $('html, body').animate({
         scrollTop: $(val[1]).offset().top - 150
     }, 2000);
+    if (val[0] === '#intro') {
+      $('tspan').each(function(index){
+          $(this).stop(true, true).delay(500* index).animate({
+              'stroke-dashoffset':0
+          }, 800);
+      })
+      setTimeout(function() {        
+        $('#babbage').fadeIn(2000);
+      }, 7000);
+    }
     return false;
 })
 })
@@ -87,4 +98,130 @@ $('#blurrMe').on('click',function() {
     $('#close').click();
   }
   
+})
+
+var canvas = $('canvas')[0];
+var context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var Dots = [];
+var colors = ['#FF9900', '#424242', '#BCBCBC', '#3299BB'];
+var maximum = 70;
+
+function Initialize() {
+  GenerateDots();
+
+  Update();
+}
+
+function Dot() {
+  this.active = true;
+
+  this.diameter = Math.random() * 7;
+
+  this.x = Math.round(Math.random() * canvas.width);
+  this.y = Math.round(Math.random() * canvas.height);
+
+  this.velocity = {
+    x: (Math.random() < 0.5 ? -1 : 1) * Math.random() * 0.7,
+    y: (Math.random() < 0.5 ? -1 : 1) * Math.random() * 0.7
+  };
+
+  this.alpha = 0.1;
+  this.hex = colors[Math.round(Math.random() * 3)];
+  this.color = HexToRGBA(this.hex, this.alpha);
+}
+
+Dot.prototype = {
+  Update: function() {
+    if(this.alpha < 0.8) {
+      this.alpha += 0.01;
+      this.color = HexToRGBA(this.hex, this.alpha);
+    }
+
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+
+    if(this.x > canvas.width + 5 || this.x < 0 - 5 || this.y > canvas.height + 5 || this.y < 0 - 5) {
+      this.active = false;
+    }
+  },
+
+  Draw: function() {
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.arc(this.x, this.y, this.diameter, 0, Math.PI * 2, false);
+    context.fill();
+  }
+}
+
+function Update() {
+  GenerateDots();
+
+  Dots.forEach(function(Dot) {
+    Dot.Update();
+  });
+
+  Dots = Dots.filter(function(Dot) {
+    return Dot.active;
+  });
+
+  Render();
+  requestAnimationFrame(Update);
+}
+
+function Render() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  Dots.forEach(function(Dot) {
+    Dot.Draw();
+  });
+}
+
+function GenerateDots() {
+  if(Dots.length < maximum) {
+    for(var i = Dots.length; i < maximum; i++) {
+      Dots.push(new Dot());
+    }
+  }
+
+  return false;
+}
+
+function HexToRGBA(hex, alpha) {
+  var red = parseInt((TrimHex(hex)).substring(0, 2), 16);
+  var green = parseInt((TrimHex(hex)).substring(2, 4), 16);
+  var blue = parseInt((TrimHex(hex)).substring(4, 6), 16);
+
+  return 'rgba(' + red + ', ' + green + ', ' + blue + ', ' + alpha + ')';
+}
+
+function TrimHex(hex) {
+  return (hex.charAt(0) == "#") ? hex.substring(1, 7) : h;
+}
+
+$(window).resize(function() {
+  Dots = [];
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+Initialize();
+
+var s=Snap('svg');
+var text='Are We Different?'
+var len=text.length;
+var array=[];
+for(var x=0;x < len;x++){
+    var t=text[x]
+    array.push(t);
+}
+var txt=s.text(50,50,array)
+$('tspan').css({
+    'font-size':50,
+    fill:'none',
+    stroke:'black',
+    'stroke-dasharray':230,
+    'stroke-dashoffset':230
 })
--- parser.java
+++ parser.java
@@ -5,111 +5,111 @@ public class Parser {
  public Parser(String input) {
  this.input = input + EOF; // mark the end
  this.currPos = -1;
- }
- public String parse() throws IllegalArgumentException {
+}
+public String parse() throws IllegalArgumentException {
  nextToken();
  Result result = expression();
  if(currToken() != EOF) {
- throw new IllegalArgumentException("Found unexpected character '"
-+ currToken() + "' at position " + currPos);
+   throw new IllegalArgumentException("Found unexpected character '"
+    + currToken() + "' at position " + currPos);
  }
  return result.getText();
- }
+}
  // "expression()" handles "term" or "term + term" or "term - term"
- private Result expression() throws IllegalArgumentException {
+private Result expression() throws IllegalArgumentException {
  Result leftArg = term();
  char operator = currToken();
  if (operator != '+' && operator != '-') {
  return leftArg; // EXIT
- }
- nextToken();
- Result rightArg = term();
- if(operator == '-' && (rightArg.getOp() == '-' || rightArg.getOp() ==
-'+')) {
+}
+nextToken();
+Result rightArg = term();
+if(operator == '-' && (rightArg.getOp() == '-' || rightArg.getOp() ==
+  '+')) {
  rightArg = encloseInParentheses(rightArg);
- }
- return new Result(leftArg.getText() + "" + operator + "" +
-rightArg.getText(), operator);
- }
+}
+return new Result(leftArg.getText() + "" + operator + "" +
+  rightArg.getText(), operator);
+}
  // "term()" handles "factor" or "factor * factor" or "factor / factor"
- private Result term() throws IllegalArgumentException {
+private Result term() throws IllegalArgumentException {
  Result leftArg = factor();
 
  char operator = currToken();
  if (operator != '*' && operator != '/') {
  return leftArg; // EXIT
- }
- nextToken();
- Result rightArg = factor();
- if(leftArg.getOp() == '+' || leftArg.getOp() == '-') {
+}
+nextToken();
+Result rightArg = factor();
+if(leftArg.getOp() == '+' || leftArg.getOp() == '-') {
  leftArg = encloseInParentheses(leftArg);
- }
- if(rightArg.getOp() == '+' || rightArg.getOp() == '-' || (operator ==
-'/' && (rightArg.getOp() == '/' || rightArg.getOp() == '*'))) {
+}
+if(rightArg.getOp() == '+' || rightArg.getOp() == '-' || (operator ==
+  '/' && (rightArg.getOp() == '/' || rightArg.getOp() == '*'))) {
  rightArg = encloseInParentheses(rightArg);
- }
- return new Result(leftArg.getText() + "" + operator + "" +
-rightArg.getText(), operator);
- }
+}
+return new Result(leftArg.getText() + "" + operator + "" +
+  rightArg.getText(), operator);
+}
  // "factor()" handles a "paren" or a "variable"
- private Result factor() throws IllegalArgumentException {
+private Result factor() throws IllegalArgumentException {
  Result result;
  if(currToken() == '(') {
- result = paren();
+   result = paren();
  } else if(Character.isDigit(currToken())) {
- result = variable();
+   result = variable();
  } else {
- throw new IllegalArgumentException("Expected variable or '(',
-found '" + currToken() + "' at position " + currPos);
+   throw new IllegalArgumentException("Expected variable or '(',
+    found '" + currToken() + "' at position " + currPos);
  }
  return result;
- }
+}
  // "paren()" handles an "expression" enclosed in parentheses
  // Called with currToken an opening parenthesis
- private Result paren() throws IllegalArgumentException {
+private Result paren() throws IllegalArgumentException {
  nextToken();
  Result result = expression();
  if(currToken() != ')') {
- throw new IllegalArgumentException("Expected ')', found '" +
-currToken() + "' at position " + currPos);
+   throw new IllegalArgumentException("Expected ')', found '" +
+   currToken() + "' at position " + currPos);
  }
  nextToken();
  return result;
- }
+}
  // "variable()" handles a variable
   // Called with currToken a variable
- private Result variable() throws IllegalArgumentException {
+private Result variable() throws IllegalArgumentException {
  Result result = new Result(Character.toString(currToken()), ' ');
  nextToken();
  return result;
- }
- private char currToken() {
+}
+private char currToken() {
  return input.charAt(currPos);
- }
- private void nextToken() {
+}
+private void nextToken() {
  if(currPos >= input.length() - 1) {
- throw new IllegalArgumentException("Unexpected end of input");
+   throw new IllegalArgumentException("Unexpected end of input");
  }
  do {
- ++currPos;
+   ++currPos;
  }
  while(currToken() != EOF && currToken() == ' ');
- }
- private Result encloseInParentheses(Result result) {
+}
+private Result encloseInParentheses(Result result) {
  return new Result("(" + result.getText() + ")", result.getOp());
- }
- private class Result {
+}
+private class Result {
  private final String text;
  private final char op;
  private Result(String text, char op) {
- this.text = text;
- this.op = op;
+   this.text = text;
+   this.op = op;
  }
  public String getText() {
- return text;
+   return text;
  }
  public char getOp() {
- return op;
+   return op;
  }
- }
+}
 }
